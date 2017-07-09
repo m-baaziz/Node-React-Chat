44dbfbb866b150b7f9086b463e8f0072247c04a3
m-baaziz <mha.baaziz@gmail.com>
2017-07-10 00:37:17 +0200

    different webpack config files for backend and frontend source code

---
+++ backend_webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('./node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './server.js',
+  target: 'node',
+  output: {
+    path: __dirname,
+    filename: 'build.js'
+  },
+  externals: nodeModules,
+  module: {
+    loaders: [{
+      test: /.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  }
+} 
